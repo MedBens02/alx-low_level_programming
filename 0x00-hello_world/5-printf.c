d173ddfaae36a59c83330d873ff8b24334f0f844
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-05 12:33:41 -0700

    6th task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function is the entry point of the program and
+ *              prints a message to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
