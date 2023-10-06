96c545e759170593a24410dfd27d632e9240fa60
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-06 13:39:56 -0700

    extra task 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point of the progra
+ *
+ * Description: This program prints all possible combinations
+ * of double-digit numbers separated by , then space
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = n + 1; m < 10; m++)
+		{
+			putchar('0' + n);
+			putchar('0' + m);
+			if (n < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
