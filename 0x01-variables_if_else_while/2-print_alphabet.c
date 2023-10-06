ebc30f85f2fc8d941fc2f162bdef24497cd24fe6
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-06 12:41:08 -0700

    3rd task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the lowercase alphabet followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l = 'a';
+
+	while (l <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
