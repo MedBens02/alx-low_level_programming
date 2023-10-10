573bdd564f8fc25defb7c144671b5483ec3f4f76
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-10 16:55:26 -0700

    4th task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase ch
+ *
+ * Return: returns 1 if c is lc otherwise always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+	return (0);
+}
