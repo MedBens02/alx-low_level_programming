3bcf14de5cc8d187167a106f30bff645c5ba06ba
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-10 17:08:42 -0700

    6

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - prints a num sign
+ *
+ * @n: number input
+ *
+ * Return: 1 and prints + if > 0
+ * 0 and prints 0 if zero
+ * -1 and prints - if < 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
