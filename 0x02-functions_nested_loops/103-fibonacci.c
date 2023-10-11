a7c504ce38c334f9bb226914c5ae356d1d364512
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-10 19:10:53 -0700

    extra 4

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry pt
+ *
+ * Description: prints sum of even Fib nums < 4mill
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned long a, b, S, eS;
+
+	while (S <= 4000000)
+	{
+		S = a + b;
+		if (S % 2 == 0)
+			eS += S;
+		a = b;
+		b = S;
+	}
+	printf("%d\n", eS);
+	return (0);
+}
