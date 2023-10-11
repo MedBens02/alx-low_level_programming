0e6176a2f1fe2f017f9f08ede721c188409d6adb
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-10 17:41:49 -0700

    10

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ */
+
+void times_table(void)
+{
+	int a, b, p;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			p = a * b;
+			if (b == 0)
+				_putchar('0');
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (p < 10)
+					_putchar(' ');
+				_putchar((p % 10) + '0');
+			}
+		}
+	}
+}
