80b740c142dac51ad50e402cfd345489458310f8
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-12 18:01:40 -0700

    8

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line on the terminal
+ *
+ * @n: number of lines
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int i = n, j = n;
+
+	while (n > 0)
+	{
+		i = j;
+		while (i > n)
+		{
+			_putchar(' ');
+			i--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		n--;
+	}
+}
+
+
