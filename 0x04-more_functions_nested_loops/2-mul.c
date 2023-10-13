72a71c79c821af0bc37df292a880283f185cf770
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-12 17:07:27 -0700

    3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 int
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: m a * b
+ */
+
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+	return (m);
+}
