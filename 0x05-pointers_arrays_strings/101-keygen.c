8ac070cf8013169d84b5d40734ab209b33a2ad56
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-16 19:10:00 -0700

    11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random pass
+ *
+ * return: 0
+ */
+
+int main(void)
+{
+	int pass;
+	char c;
+
+	srand(time(0));
+
+	while (pass <= 4329)
+	{
+		c = rand() % 128;
+		pass *= c;
+		putchar(c);
+	}
+	putchar(4456 - pass);
+	return (0);
+}
