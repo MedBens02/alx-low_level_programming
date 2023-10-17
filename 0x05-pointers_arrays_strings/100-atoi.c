24bba074db35758ce1723edf943f0af17b45ca25
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-16 18:48:02 -0700

    10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,18 +13,15 @@ int _atoi(char *s)
 	int sign = 1;
 	unsigned int n = 0;
 
-	while (*s == ' ' || *s == '+' || *s == '-' || *s <= 95)
-	{
+	do {
 		if (*s == '-')
 			sign *= -1;
-		s++;
-	}
 
-	while (*s >= '0' && *s <= '9')
-	{
-		n = n * 10 + (*s - '0');
-		s++;
-	}
+		else if (*s >= '0' && *s <= '9')
+			n = n * 10 + (*s - '0');
+		else if (n > 0)
+			break;
+	} (*s++;)
 	n = n * sign;
 
 	return (n);
