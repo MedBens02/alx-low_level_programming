80696d62379814ecf8445a053a24f84ca1247e03
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-16 18:30:59 -0700

    10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer.
+ *
+ * @s: str input
+ *
+ * Return: n
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	int n = 0;
+
+	while (*s == ' ' || *s == '+' || *s == '-' || (*s >= 9 && *s <= 13))
+	{
+		if (*s == '-')
+			sign *= -1;
+		s++;
+	}
+
+	while (*s >= '0' && *s <= '9')
+	{
+		n = n * 10 + (*s - '0');
+		s++;
+	}
+	n = n * sign;
+
+	return (n);
+}
