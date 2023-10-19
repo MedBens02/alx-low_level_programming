6d5ef1625c1e48fab93b96fdda0cda0f9f84efc6
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-19 14:00:06 -0700

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337.
+ *
+ * @str: string input
+ *
+ * Return: coded string
+ */
+
+char *leet(char *)
+{
+	char *p = str;
+	char *chars = "AaEeOoTtLl";
+	char *codes = "4433007711";
+
+	while (*p != '\0')
+	{
+		char *check = chars;
+		char *flip = codes;
+
+		while(*check != '\0')
+		{
+			if (*p == *check)
+			{
+				*p = *check;
+				break;
+			}
+			check++;
+			flip++;
+		}
+		p++;
+	}
+	return (str);
+}
