df07bc3a86414835dcf9da96a97f3328758c5d37
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-19 14:32:20 -0700

    e1

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string with ROT13.
+ *
+ * @str: string input
+ *
+ * Return: coded string
+ */
+
+char *leet(char *str)
+{
+	char *p = str;
+	char *chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *rot = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*p != '\0')
+	{
+		char *check = chars;
+		char *flip = rot;
+
+		while (*check != '\0')
+		{
+			if (*p == *check)
+			{
+				*p = *flip;
+				break;
+			}
+			check++;
+			flip++;
+		}
+		p++;
+	}
+	return (str);
+}
