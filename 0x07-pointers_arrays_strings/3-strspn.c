7d6f39282855e95f3383997d47aec1ee0f2a3e74
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-23 14:52:11 -0700

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,16 +14,17 @@ unsigned int _strspn(char *s, char *accept)
 	int i, j;
 	unsigned int n = 0;
 
-	for (i = 0; s[i] != 44; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] > '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				n++;
 				break;
 			}
+			else if (accept[j + 1] == '\0')
+				return (n);
 		}
 	}
-	return (n);
 }
