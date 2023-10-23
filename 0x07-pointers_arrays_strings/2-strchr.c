67328505da5b1707330080aaa6736161f349615f
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-23 13:58:23 -0700

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string.
+ *
+ * @s: string.
+ * @c: char to locate.
+ *
+ * Return: pointer to the first occurrence of the character c in s
+ * and NULL if not found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			break;
+		s++;
+	}
+	return (s);
+}
