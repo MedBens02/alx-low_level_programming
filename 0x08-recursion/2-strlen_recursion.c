84a013ca09cf0dc959cb41911c8bedc36ddb6a4f
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-25 16:13:27 -0700

    2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ *
+ * %s: str.
+ *
+ * Return: length of str.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (_strlen_recursion (s + 1) + 1);
+}
