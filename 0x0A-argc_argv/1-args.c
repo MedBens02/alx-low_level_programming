81bf0b4a0c9035a3bf4803fc5f30d5d3d762bff3
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-27 17:04:39 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mian - entry
+ *
+ * @argc: int
+ * @argv: char
+ *
+ * Return: Always (0);
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
