ff170650bbb3d6535b15555b2fa41123f0d01951
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-27 17:01:11 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - start point.
+ *
+ * @argc: int
+ * @argv: int
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc)
+		printf("%s\n",  argv[0]);
+	return (0);
+}
