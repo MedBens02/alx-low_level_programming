66211eedc5a4896ef92dde2c98843723c04eac31
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-31 10:09:37 -0700

    e0

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+/**
+ * argstostr - concatenates all the arguments of your program.
+ *
+ * @ac: num of args
+ * @av: vec args
+ * @s: str
+ *
+ * Return: ptr to result
+ */
+
+int _strlen(char *str);
+
+char *argstostr(int ac, char **av)
+{
+	char *p;
+	int i, j, p_len = 0, p_i = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		if (av[i] != NULL)
+			p_len +=(1 +  _strlen(av[i]));
+	}
+
+	p = malloc(sizeof(*p) * p_len + 1);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		if (av[i] != NULL)
+		{
+			for (j = 0; av[i][j] != '\0'; j++, p_i++)
+				p[p_i] = av[i][j];
+
+			p[p_i] = '\n';
+			p_i++;
+		}
+	}
+
+	p[p_i] = '\0';
+
+	return (p);
+}
+
+/**
+ * _strlen - calculates a str lenght.
+ *
+ * @s: str
+ *
+ * Return: len of str
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	return (len);
+}
