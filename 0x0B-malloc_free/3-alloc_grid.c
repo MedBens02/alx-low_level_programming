60fbd463ec73fd34f639dfb3c781f598ab25f10e
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-10-31 09:24:47 -0700

    3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * alloc_grid - 2 dimensional array of integers.
+ *
+ * @width: int
+ * @height: int
+ *
+ * Return: ptr to 2D array, NULL if failed.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	**p = malloc(height * sizeof(*p));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		p[i] = malloc(width * sizeof(**p));
+		if (p[i] == NULL)
+			return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			p[i][j] = 0;
+	}
+
+	return (p)
+}
