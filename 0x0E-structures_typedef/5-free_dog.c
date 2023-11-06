4402f4bfebb3fd67bf668af9f2b059460e14f3c7
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-11-06 15:10:43 -0800

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ *
+ * @d: dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			free(d->name);
+		if (d->owner != NULL)
+			free(d->owner);
+		free(d);
+	}
+}
