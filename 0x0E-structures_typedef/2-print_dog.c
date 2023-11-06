943dbc6288771363dc0e66ff14f24c0d26b677d2
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-11-06 14:27:37 -0800

    2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints a dog's info
+ *
+ * @d: dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	}
+}
