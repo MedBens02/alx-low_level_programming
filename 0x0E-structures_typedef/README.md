0abef0a435f1d804c3b43e68c26e91afc72b4ff9
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-11-06 14:09:49 -0800

    0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+new struct project
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - a dog's info
+ *
+ * @name: its name
+ * @age: its age
+ * @owner: its owner
+ *
+ * Description: a dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
