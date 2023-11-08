e41bb4507bf4e5f143a5614880b6c506dee78722
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-11-08 11:20:30 -0800

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer.
+ *
+ * @array: array of ints
+ * @size: nbr of elts in array
+ * @cmp: ptr to fct to compare values.
+ *
+ * Return: index of the first elt for which the cmp function does not return 0
+ * if no match or size <= 0 returns -1.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	if (array && size && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+		return (-1);
+	}
+	return (0);
+}
