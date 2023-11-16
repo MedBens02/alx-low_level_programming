61c7f11e5583f405461787395452cc5d84a6ce2d
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-11-16 15:00:35 -0800

    0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements of a list
+ *
+ * @h: ptr to head of the list
+ *
+ * Return: num of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		n++;
+
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+	}
+
+	return (n);
+}
