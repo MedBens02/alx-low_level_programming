2a29d54f1471c438db6b7e21095729c8899eea2d
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-11-16 15:04:12 -0800

    1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements of a list
+ *
+ * @h: ptr to head of the list
+ *
+ * Return: num of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+
+	return (n);
+}
