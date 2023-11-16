749866cb632fc0eb2d133ef87ca187cb6d95813d
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-11-16 15:54:16 -0800

    4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ *
+ * @head: head of the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr->str);
+		free(ptr);
+	}
+}
