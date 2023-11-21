638a5bf198eebacc04c02e382dba8c15dd5a7060
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-11-21 10:47:59 -0800

    5

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ *
+ * @head: head of the list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr);
+	}
+}
