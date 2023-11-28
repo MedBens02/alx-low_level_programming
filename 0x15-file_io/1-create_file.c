f640377f3439b423f35031be30e0df8ccd000133
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-11-27 17:19:02 -0800

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -45,7 +45,7 @@ int create_file(const char *filename, char *text_content)
 		bytes = write(fd, text_content, l);
 
 	close(fd);
-	if (bytes == len)
+	if (bytes == l)
 		return (1);
 	else
 		return (-1);
