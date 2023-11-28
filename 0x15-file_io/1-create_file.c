41bdde81915b75b58ef81a73edbb0caf1108edc6
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-11-27 17:18:22 -0800

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _strlen - len of a str
+ *
+ * @s: str
+ *
+ * Return: len
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	if (str == NULL)
+		return (0);
+	while (*s)
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * create_file - creates a file
+ *
+ * @filename: its name
+ * @text_content: txt to write
+ *
+ * Return: 1 success, 0 fail
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, l = _strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (len != NULL)
+		bytes = write(fd, text_content, l);
+
+	close(fd);
+	if (bytes == len)
+		return (1);
+	else
+		return (-1);
+}
