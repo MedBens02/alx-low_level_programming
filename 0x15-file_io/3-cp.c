dcde0efa452393157ce97b63360ff5277f110568
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2023-11-27 18:04:25 -0800

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 #define USAGE "Usage: cp file_from file_to\n"
-#define ER_NOREAD "Error: Can't read from file\n"
-#define ER_NOWRITE "Error: Can't write to\n"
+#define ER_NOREAD "Error: Can't read from file %s\n"
+#define ER_NOWRITE "Error: Can't write to %s\n"
 #define ER_NOCLOSE "Error: Can't close fd %d\n"
 #define PERM (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 #define BUFFER_SIZE 1024
