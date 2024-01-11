e73f7d5ae6750f8ada0dae843de6432b015550ad
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-01-11 14:59:15 -0800

    2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,15 +8,19 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	list_t *ptr;
+	dlistint_t *ptr;
 
-	ptr = malloc(sizeof(list_t));
+	ptr = malloc(sizeof(dlistint_t));
 	if (ptr == NULL)
 		return (NULL);
 
 	ptr->n = n;
 	ptr->prev = NULL;
 	ptr->next = *head;
+
+	if (*head != NULL)
+		(*head)->prev = ptr;
+
 	*head = ptr;
 
 	return (ptr);
