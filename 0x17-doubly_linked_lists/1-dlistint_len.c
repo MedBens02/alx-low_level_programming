44c33d9e240438927cb53134453822362732f7ee
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-01-11 14:45:33 -0800

    1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - calculates len of a dl list
+ * @h: ptr to head of the list
+ * Return: len
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+
+	return (n);
+}
