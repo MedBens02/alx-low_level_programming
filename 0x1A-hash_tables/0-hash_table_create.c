2e6a4e7fd633fc4f6ceaa25eca309c6a3a2b9d62
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-02-02 13:59:34 -0800

    0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table.
+ *
+ * @size: size of the table.
+ * Return: Pointer to the hash table or NULL.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+
+	ht->size = size;
+
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
+}
