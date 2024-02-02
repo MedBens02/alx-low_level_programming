80fe0174ce3f6fc975023a3c9da6cfe69e5f0e30
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-02-02 14:09:01 -0800

    2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Gives the index of a key.
+ *
+ * @key: key to search for.
+ * @size: size of the array.
+ *
+ * Return: index at which the key/value pair should be stored.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
