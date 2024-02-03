8832d22fad104f977cea9fff408bdb8422c379e4
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-02-02 16:58:33 -0800

    5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (flag != 0)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			flag = 0;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
